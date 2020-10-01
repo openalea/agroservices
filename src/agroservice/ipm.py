0348399af5f14606e7c57a4126cff2092cb313d9
mlabadie <labadiemarc89@gmail.com>
2020-10-01 12:31:18 +0200

    add all function for get weatherdata ressource

--- src/agroservice/ipm.py
+++ src/agroservice/ipm.py
@@ -5,105 +5,6 @@
 #
 # ==============================================================================
 
-""" Web service to GET and POST data to phis v1 """
-
-# ==============================================================================
-import urllib
-import requests
-import six
-
-# ==============================================================================
-
-_ws_address = 'https://ipmdecisions.nibio.no/api/wx/rest/'
-
-
-def post_json(address, web_service, json_txt, timeout=10., overwriting=False, **kwargs):
-    """ Function calling a web service
-
-    :param address: (str) network address of the service web
-    :param web_service: (str) name of web service requested
-    :param json_txt: (str) data formatted as json
-    :param timeout: (float) timeout for connexion in seconds
-    :param overwriting: (bool) allowing to overwrite data or not
-
-    :return
-        (dict) response of the server (standard http)
-        (bool) whether data has been overwrote or not
-    """
-    overwrote = False
-    headers = {"Content-type": "application/json"}
-    response = requests.request(method='POST', url=address + "/" + web_service, headers=headers, data=json_txt,
-                                params=kwargs, timeout=timeout)
-    if response.status_code == 200 and overwriting:
-        response = requests.request(method='PUT', url=address + "/" + web_service, headers=headers, data=json_txt,
-                                    params=kwargs, timeout=timeout)
-        overwrote = True
-    return response, overwrote
-
-
-def get(address, web_service, timeout=10., **kwargs):
-    """
-
-    :param address: (str) network address of the service web
-    :param web_service: (str) name of web service requested
-    :param timeout: (float) timeout for connexion in seconds
-    :param kwargs: (str) arguments relative to web service (see http://147.99.7.5:8080/phenomeapi/api-docs/#/)
-    :return:
-        (dict) response of the server (standard http)
-    """
-    response = requests.request(method='GET', url=address + "/" + web_service, params=kwargs, timeout=timeout)
-    return response
-
-
-def get_all_data(address, web_service, timeout=10., **kwargs):
-    """
-
-    :param address:  (str) network address of the service web
-    :param web_service:  (str) name of web service requested
-    :param timeout:  (float) timeout for connexion in seconds
-    :param kwargs: (str) arguments relative to web service (see http://147.99.7.5:8080/phenomeapi/api-docs/#/)
-    :return:
-        (list of dict) data relative to web service and parameters
-    """
-    current_page = 0
-    total_pages = 1
-    values = list()
-
-    # TODO remove 'plants' specificity as soon as web service delay fixed
-    kwargs['pageSize'] = 10
-
-    while total_pages > current_page:
-        kwargs['page'] = current_page
-        response = requests.request(method='GET', url=address + "/" + web_service, params=kwargs, timeout=timeout)
-        if response.status_code == 200:
-            values.extend(response.json())
-        elif response.status_code == 500:
-            raise Exception("Server error")
-        else:
-            raise Exception(response.json()["metadata"]["status"][0]["message"])
-
-        if response.json()["metadata"]["pagination"] is None:
-            total_pages = 0
-        else:
-            total_pages = response.json()["metadata"]["pagination"]["totalPages"]
-        current_page += 1
-
-    return values
-
-def parameter():
-    return get(address= _ws_address, web_service="parameter").json()
-
-def weatherdatasource():
-    return get(address= _ws_address, web_service="weatherdatasource").json()
-
-def weatheradapter_fmi(ignoreErrors=True,interval=3600,parameters='1001',timeEnd= '2020-07-03T00:00:00+03:00',timeStart= "2020-06-12T00:00:00+03:00",weatherStationId='101690'):
-    return get_all_data(address= _ws_address, web_service="weatheradapter/fmi",ignoreErrors=ignoreErrors,interval=interval,parameters=parameters,timeEnd= timeEnd,timeStart= timeStart,weatherStationId=weatherStationId).json()
-
-def weatheradapter_yr(altitude = 56, latitude=43.36 ,longitude=3.52 ):
-    return get(address= _ws_address, web_service="weatheradapter/yr", altitude=altitude, longitude = longitude, latitude=latitude).json()
-
-
-
 
 ################## Interface Python IPM using Bioservice ########################################################
 
@@ -111,9 +12,36 @@ from bioservices.services import REST
 
 __all__ = ["IPM"]
 
-class IPM():
+class IPM(REST):
     """
     Interface to the IPM weather https://ipmdecisions.nibio.no/api/wx/rest
+
+    ..doctest::
+        >>> from agroservice.ipm import IPM
+        >>> ipm = IPM()
+
+        MetaDataService
+        ----------------
+        >>> ipm.get_parameter()
+        >>> ipm.get_qc()
+        >>> ipm.get_schema_weatherdata() TypeError:get_schema_weatherdata() takes 0 positional arguments but 1 was given
+        >>> ipm.post_schema_weatherdata_validate() TODO
+        
+        WeatherAdaptaterService
+        ------------------------
+        >>> ipm.get_weatheradapter_fmi(ignoreErrors=True,interval= 3600,parameters=1001,timeEnd= "2020-07-03T00:00:00+03:00",timeStart= "2020-06-12T00:00:00+03:00",weatherStationId=101533)
+        >>> ipm.post_weatheradapter_fmi() TODO
+        >>> ipm.get_weatheradapter_fmi_forecasts(latitude=43.36, longitude=3.52)
+        >>> ipm.post_weatheradapter_fmi_forecasts() TODO
+        >>> ipm.get_weatheradapter_yr(altitude=56, latitude=43.36, longitude=3.52)
+        >>> ipm.post_weatheradapter_yr() TODO
+
+        WeatherDataService
+        ------------------
+        >>> ipm.get_weatherdatasource()
+        >>> ipm.post_weatherdatasource_location(tolerence=0) TODO
+        >>> ipm.get_weatherdatasource_location_point(latitude=43.36, longitude=3.52,tolerence=0)
+
     """
 
     _url = "https://ipmdecisions.nibio.no/api/wx/rest"
@@ -132,8 +60,10 @@ class IPM():
     ########################## MetaDataService ##########################################
        
     # Parameters
-    def get_parameter(self,frmt='json'):
+    def get_parameter(self, frmt='json'):
         """
+        Get a list of all the weather parameters defined in the platform
+
         parameters:
         -----------
 
@@ -142,14 +72,16 @@ class IPM():
             a list of all the weather parameters used in the platform in json format
         """    
 
-        res = self.services.http_get("parameter", frmt=frmt,
-                headers=self.services.get_headers(content=frmt),
+        res = self.services.http_get("parameter", frmt='json',
+                headers=self.services.get_headers(content='json'),
                 params={'callback':self.callback})
         return res
     
     # QC
     def get_qc(self,frmt='json'):
         """
+        Get a list of QC code
+
         parameters:
         -----------
 
@@ -167,6 +99,9 @@ class IPM():
 
     def get_schema_weatherdata():
         """
+        Get a schema that describes the IPM Decision platform's format for exchange of weather data
+        Warning: TypeError: get_schema_weatherdata() takes 0 positional arguments but 1 was given
+        
         parameters:
         ------------
 
@@ -182,7 +117,19 @@ class IPM():
     # schema weather data validate
 
     def post_schema_weatherdata_validate():
-        pass       
+        '''
+        Validates the posted weather data against the Json schema
+
+        parameters:
+        -----------
+            data: in json format
+
+        return:
+        -------
+            {"isValid":"true"} if the data is valid, {"isValid":"false"} otherwise
+        '''
+        res = self.services.http_post('/schema/weatherdata/validate',frmt='json',data=None)
+        return res 
 
     ###################### WeatherAdaptaterService #############################
 
@@ -190,13 +137,15 @@ class IPM():
 
     def get_weatheradapter_fmi(self,frmt="json",ignoreErrors="ignoreErrors",interval= "interval",parameters="parameters",timeEnd= "timeEnd",timeStart= "timeStart",weatherStationId="weatherStationId"):
         """
+        Get weather observations in the IPM Decision's weather data format from the Finnish Meteorological Institute https://en.ilmatieteenlaitos.fi/ Access is made through the Institute's open data API: https://en.ilmatieteenlaitos.fi/open-data
+       
         parameters:
         -----------
         ignoreErrors: (Bolean) Set to "true" if you want the service to return weather data regardless of there being errors in the service
         interval: (int) he measuring interval in seconds. Please note that the only allowed interval in this version is 3600 (hourly)
-        parameters: (list)  Comma separated list of the requested weather parameters
-        timeStart: Start of weather data period (ISO-8601 Timestamp, e.g. 2020-06-12T00:00:00+03:00)
-        timeEnd: End of weather data period (ISO-8601 Timestamp, e.g. 2020-07-03T00:00:00+03:00)
+        parameters: (string of  Comma separated list) of the requested weather parameters
+        timeStart: (string) Start of weather data period (ISO-8601 Timestamp, e.g. 2020-06-12T00:00:00+03:00)
+        timeEnd: (string) End of weather data period (ISO-8601 Timestamp, e.g. 2020-07-03T00:00:00+03:00)
         weatherStationId: The weather station id (FMISID) in the open data API https://en.ilmatieteenlaitos.fi/observation-stations?filterKey=groups&filterQuery=weather
 
         Returns:
@@ -207,7 +156,7 @@ class IPM():
         res = self.services.http_get("weatheradapter/fmi", frmt=frmt,
                 headers=self.services.get_headers(content=frmt),
                 params={'callback':self.callback,
-                "ignoreErrors":ignoreErrors,"interval":interval,"parameters":parameters,"timeEnd":timeEnd,"timeStart":timeStart,"weatherStationId":weatherStationId})
+                "ignoreErrors":ignoreErrors,"interval":int(interval),"parameters":parameters,"timeEnd":timeEnd,"timeStart":timeStart,"weatherStationId":int(weatherStationId)})
         return res
     
     def post_weatheradapter_fmi():
@@ -224,12 +173,13 @@ class IPM():
         Returns:
         --------
          weather observations in the IPM Decision's weather data format from the Finnish Meteorological Institute https://en.ilmatieteenlaitos.fi/ in json format
-
         """
         pass
 
     def get_weatheradapter_fmi_forecasts(self,frmt='json',latitude="latitude", longitude="longitude"):
         """
+        Get 36 hour forecasts from FMI (The Finnish Meteorological Institute), using their OpenData services at https://en.ilmatieteenlaitos.fi/open-data
+        
         parameters:
         -----------
             latitude: (double) WGS84 Decimal degrees
@@ -257,11 +207,13 @@ class IPM():
             36 hour forecasts from FMI (The Finnish Meteorological Institute), using their OpenData services at https://en.ilmatieteenlaitos.fi/open-data
             the weather forecast formatted in the IPM Decision platform's weather data format
         """
-        pass
+        
 
     # weatheradapter_yr
-    def get_weatheradapter_yr(self, frmt="json",altitude="altitude",longitude="longitude",latitude="latitude"):
+    def get_weatheradapter_yr(self, frmt="json",altitude=56,longitude=3.52,latitude=43.36):
         """
+        Get 9 day weather forecasts from The Norwegian Meteorological Institute's Locationforecast API
+
         parameters:
         -----------
             altitute: (double) Meters above sea level. This is used for correction of temperatures (outside of Norway, where the local topological model is used) eg:56
@@ -299,6 +251,8 @@ class IPM():
 
     def get_weatherdatasource(self,frmt='json'):
         """
+        Get a list of all the available weather data sources
+
         parameters:
         -----------
 
@@ -306,7 +260,7 @@ class IPM():
         --------
         return list of all the available weather data sources in json
         """
-        res = self.services.http_get("weatherdatasource", frmt=frmt,
+        res = self.services.http_get("/weatherdatasource", frmt=frmt,
                 headers=self.services.get_headers(content=frmt),
                 params={'callback':self.callback})
         return res
