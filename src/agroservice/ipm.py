228b43693fe79de1f11fda360cd9abd55d1343fc
mlabadie <labadiemarc89@gmail.com>
2020-10-02 14:05:40 +0200

    add IPM_DSS class

--- src/agroservice/ipm.py
+++ src/agroservice/ipm.py
@@ -10,38 +10,37 @@
 
 from bioservices.services import REST
 
-__all__ = ["IPM"]
+__all__ = ["IPM_weather","IPM_DSS"]
 
-class IPM(REST):
+class IPM_weather(REST):
     """
     Interface to the IPM weather https://ipmdecisions.nibio.no/api/wx/rest
 
     ..doctest::
         >>> from agroservice.ipm import IPM
-        >>> ipm = IPM()
+        >>> ws = IPM_weather()
 
         MetaDataService
         ----------------
-        >>> ipm.get_parameter()
-        >>> ipm.get_qc()
-        >>> ipm.get_schema_weatherdata() TypeError:get_schema_weatherdata() takes 0 positional arguments but 1 was given
-        >>> ipm.post_schema_weatherdata_validate() TODO
+        >>> ws.get_parameter()
+        >>> ws.get_qc()
+        >>> ws.get_schema_weatherdata() TypeError:get_schema_weatherdata() takes 0 positional arguments but 1 was given
+        >>> ws.post_schema_weatherdata_validate() TODO
         
         WeatherAdaptaterService
         ------------------------
-        >>> ipm.get_weatheradapter_fmi(ignoreErrors=True,interval= 3600,parameters=1001,timeEnd= "2020-07-03T00:00:00+03:00",timeStart= "2020-06-12T00:00:00+03:00",weatherStationId=101533)
-        >>> ipm.post_weatheradapter_fmi() TODO
-        >>> ipm.get_weatheradapter_fmi_forecasts(latitude=43.36, longitude=3.52)
-        >>> ipm.post_weatheradapter_fmi_forecasts() TODO
-        >>> ipm.get_weatheradapter_yr(altitude=56, latitude=43.36, longitude=3.52)
-        >>> ipm.post_weatheradapter_yr() TODO
+        >>> ws.get_weatheradapter_fmi(ignoreErrors=True,interval= 3600,parameters=1001,timeEnd= "2020-07-03T00:00:00+03:00",timeStart= "2020-06-12T00:00:00+03:00",weatherStationId=101533)
+        >>> ws.post_weatheradapter_fmi() TODO
+        >>> ws.get_weatheradapter_fmi_forecasts(latitude=43.36, longitude=3.52)
+        >>> ws.post_weatheradapter_fmi_forecasts() TODO
+        >>> ws.get_weatheradapter_yr(altitude=56, latitude=43.36, longitude=3.52)
+        >>> ws.post_weatheradapter_yr() TODO
 
         WeatherDataService
         ------------------
-        >>> ipm.get_weatherdatasource()
-        >>> ipm.post_weatherdatasource_location(tolerence=0) TODO
-        >>> ipm.get_weatherdatasource_location_point(latitude=43.36, longitude=3.52,tolerence=0)
-
+        >>> ws.get_weatherdatasource()
+        >>> ws.post_weatherdatasource_location(tolerence=0) TODO
+        >>> ws.get_weatherdatasource_location_point(latitude=43.36, longitude=3.52,tolerence=0)
     """
 
     _url = "https://ipmdecisions.nibio.no/api/wx/rest"
@@ -52,7 +51,7 @@ class IPM(REST):
     
         """
         
-        self.services = REST(name="IPM", url=IPM._url,
+        self.services = REST(name="IPM_weather", url=IPM_weather._url,
             verbose=verbose, cache=cache)
         
         self.callback = None #use in all methods)
@@ -97,7 +96,7 @@ class IPM(REST):
     
     # schema weather data
 
-    def get_schema_weatherdata():
+    def get_schema_weatherdata(self,frmt='json'):
         """
         Get a schema that describes the IPM Decision platform's format for exchange of weather data
         Warning: TypeError: get_schema_weatherdata() takes 0 positional arguments but 1 was given
@@ -296,6 +295,210 @@ class IPM(REST):
                 headers=self.services.get_headers(content=frmt),
                 params={'callback':self.callback, "latitude":latitude, "longitude":longitude, "tolerance":tolerance})
         return res
-    
 
+class IPM_DSS(REST):
+
+    """
+    Interface to the IPM DSS https://ipmdecisions.nibio.no/api/dss/rest/
+
+    ..doctest::
+        >>> from agroservice.ipm import IPM_DSS
+        >>> dss = IPM_DSS()
+
+        DSSService
+        ----------
+        >>> dss.get_crop()
+        >>> dss.get_dss()
+        >>> dss.get_pest()
+        >>> dss.post_dss_location()
+        >>> dss.get_dssId()
+        >>> dss.get_cropCode()
+        >>> dss.get_dss_location_point()
+        >>> dss.get_pestCode()
+        >>> dss.get_model()
+
+        MetaDataService
+        ---------------
+        >>> dss.get_schema_fieldobservation()
+        >>> dss.get_schema_modeloutput()
+        >>> dss.post_schema_modeloutput_validate()
+
+    """
+
+    _url = 'https://ipmdecisions.nibio.no/api/dss/rest'
+
+    def __init__(self, verbose=False, cache=False):
+        """**Constructor**
+        :param verbose: set to False to prevent informative messages
     
+        """
+        
+        self.services = REST(name="IPM_DSS", url=IPM_DSS._url,
+            verbose=verbose, cache=cache)
+        
+        self.callback = None #use in all methods)
+    
+###########################   DSSService  ################################################
+
+    def get_crop(self,frmt='json'):
+        """
+        Get a list of EPPO codes for all crops that the DSS models in plateform
+
+        Parameters:
+        -----------
+
+        Returns:
+        ---------
+            A list of EPPO codes https://www.eppo.int/RESOURCES/eppo_databases/eppo_codes) for all crops that the DSS models in the platform
+        """
+
+        res = self.services.http_get("crop",frmt='json',
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback})
+        return res
+
+
+    def get_dss(self,frmt='json'):
+        """
+        Get a list all DSSs and models available in the platform
+        
+        Parameters:
+        -----------
+
+        Returns:
+        --------
+           a list all DSSs and models available in the platform     
+        """
+        res = self.services.http_get("dss",frmt=frmt,
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback})
+        return res
+
+    def get_pest(self,frmt='json'):
+        """
+        Get A list of EPPO codes https://www.eppo.int/RESOURCES/eppo_databases/eppo_codes) for all pests that the DSS models in the platform deals with in some way.
+        
+        Parameters:
+        -----------
+
+        Returns:
+        --------
+            A list of EPPO codes https://www.eppo.int/RESOURCES/eppo_databases/eppo_codes) for all pests that the DSS models in the platform deals with in some way.
+        """
+        res = self.services.http_get("pest",frmt=frmt,
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback})
+        return res
+    
+    def post_dss_location():
+        pass
+    
+    def get_dssId(self, frmt='json',DSSId='DSSId'):
+        """ 
+        Get all information about a specific DSS
+
+        Parameters:
+        -----------
+            DSSId: (path) the id of the DSS
+        
+        Returns:
+        --------
+            DSS(JSON) the requested DSS
+        """
+        res = self.services.http_get("dss/{}".format(DSSId),frmt=frmt)
+        return res
+    
+    def get_cropCode(self,frmt='json',cropCode='cropCode'):
+        res = self.services.http_get("dss/crop/{}".format(cropCode),frmt=frmt)
+        return res
+    
+    def get_dss_location_point(self, frmt='json',latitude = 'latitude', longitude= 'longitude'):
+        """ 
+        Search for models that are valid for the specific point
+
+        Parameters:
+        -----------
+            latitude: (double) in decimal degrees (WGS84)
+            longitude: (double) in decimal degrees (WGS84)
+        
+        Returns:
+        --------
+            A list of all the matching DSS models (array of DSS (JSON))
+        
+        """
+        res = self.services.http_get("dss/location/point",frmt=frmt,
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback,'latitude':latitude,'longitude':longitude})
+        return res
+    
+    def get_pestCode(self,frmt='json',pestCode='pestCode'):
+        """ 
+        Returns a list of models that are applicable to the given pest
+
+        Parameters:
+        -----------
+            pestCode: (path) EPPO code for the pest https://www.eppo.int/RESOURCES/eppo_databases/eppo_codes
+        
+        Returns:
+        --------
+            a list of models that are applicable to the given pest (array of DSS (JSON))
+        """
+        res = self.services.http_get('dss/pest/{}'.format(pestCode),frmt='json')
+        return res
+        
+    def get_model(self,frmt='json',DSSId='DSSId',ModelId='ModelId'):
+        """ 
+        Get all information about a specific DSS model
+
+        Parameters:
+        -----------
+            DSSId: (path) The id of the DSS containing the model
+            ModelId: (path) The id of the DSS model requested
+        
+        Returns:
+        --------
+            The requested DSS model (DSSModel (JSON))
+        
+        """
+        res = self.services.http_get("model/{}/{}".format(DSSId,ModelId),frmt=frmt)
+        return res
+
+###############################  MetaDataService ##############################################
+
+    def get_schema_fieldobservation(self, frmt='json'):
+        """
+        Get the generic schema for field observations, containing the common properties for field observations. 
+        These are location (GeoJson), time (ISO-8859 datetime), EPPO Code for the pest and crop. 
+        In addition, quantification information must be provided. 
+        This is specified in a custom schema, which must be a part of the input_schema property in the DSS model metadata. 
+
+        Parameters:
+        -----------
+
+        Returns:
+        --------
+            The generic schema for field observations (object(JSON))
+        """
+        res = self.services.http_get("schema/fieldobservation",frmt=frmt,
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback})
+        return res
+
+    def get_schema_modeloutput(self, frmt='json'):
+        """
+        Get The Json Schema for the platform's standard for DSS model output
+
+        Parameters:
+        -----------
+
+        Returns:
+        --------
+            The Json Schema for the platform's standard for DSS model output (object (JSON))
+        """
+        res = self.services.http_get("schema/modeloutput",frmt=frmt,
+                                    headers=self.services.get_headers(content=frmt),
+                                    params={'callback':self.callback})
+        return res
+    
+    def post_schema_modeloutput_validate():
+        pass
