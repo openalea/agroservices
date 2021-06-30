8aacf73f4fae0899fdfc05b60a0fc19a844750b3
Marc LABADIE <labadiemarc89@gmail.com>
2021-06-30 16:46:19 +0200

    remove import module in init

--- src/agroservices/__init__.py
+++ src/agroservices/__init__.py
@@ -17,23 +17,23 @@ try:
 except:
     version = __version__
 
-import colorlog
-logger = colorlog.getLogger("agroservices")
+#import colorlog
+#logger = colorlog.getLogger("agroservices")
 
-from .extern.easydev.config_tools import CustomConfig
+#from .extern.easydev.config_tools import CustomConfig
 
 # Initialise the config directory if not already done
-configuration = CustomConfig("agroservices", verbose=False)
-bspath = configuration.user_config_dir
+#configuration = CustomConfig("agroservices", verbose=False)
+#bspath = configuration.user_config_dir
 
-from . import settings
-from .settings import *
+#from . import settings
+#from .settings import *
 
-from . import services
-from .services import *
+#from . import services
+#from .services import *
 
-from . import ipm
-from .ipm import *
+#from . import ipm
+#from .ipm import *
 
 
 # sub packages inside bioservices.
